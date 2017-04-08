68eae2a4a99d5613cf2b929971c7bfcfa3f94634
Philip Trauner <philip.trauner@aol.com>
2017-04-08 19:14:24 +0200

    Moved Google Analytics from template/index.html

--- static/script.js
+++ static/script.js
@@ -1,12 +1,19 @@
-var location_set = false;
+var locationSet = false;
 if (location.href.substring(location.href.indexOf("#")+1) == location.href) {
 	location.href += "#about";
-	location_set = true;
+	locationSet = true;
 }
-if (!location_set) {
-	current_location = location.href.split("#");
-	location.href = current_location[0] + "#" + current_location[1];
+if (!locationSet) {
+	currentLocation = location.href.split("#");
+	location.href = currentLocation[0] + "#" + currentLocation[1];
 }
 window.addEventListener("hashchange", function(event) {
 	window.scrollTo(0, 0);
 }, false);
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-38226657-2', 'auto');
+ga('send', 'pageview');
