d184bebf090414b42f7d2cbafa657e03c4dbdf2d
Philip Trauner <philip.trauner@aol.com>
2017-11-26 21:04:10 +0100

    Updated dependencies, reordered imports, removed auto 'npm run build'

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from Meh import Config, Option, ExceptionInConfigError
-from GitHubBridge import GitHub
-from sanic import Sanic
-from sanic.response import html
-import jinja2
-import pickle
 from os.path import exists
 from os import system
+from pickle import dumps as pickle_dumps
+from pickle import loads as pickle_loads
+
+from meh import Config, Option, ExceptionInConfigError
+from github_bridge import GitHub
+from sanic import Sanic
+from sanic.response import html
+from jinja2 import Template as JinjaTemplate
+
 
 NAME = "philip-trauner.me"
 CONFIG_PATH = "philip-trauner.me.cfg"
@@ -13,13 +16,6 @@ DUMP_PATH = "github-dump.pickle"
 
 PATH_HTML = "templates/index.min.html"
 
-REQUIRED_STATIC_CONTENT = [PATH_HTML, "static/style.min.css", "static/script.min.js"]
-
-for path in REQUIRED_STATIC_CONTENT:
-	if not exists(path):
-		print("You forgot to run 'npm run build'. Let me run it for you :)")
-		system("npm run build")
-		break
 
 config = Config()
 config += Option("address", "0.0.0.0")
@@ -44,7 +40,7 @@ if config.static_url != "":
 
 static_url = str(config.static_url)
 
-template = jinja2.Template(open(PATH_HTML).read())
+template = JinjaTemplate(open(PATH_HTML).read())
 app = Sanic(NAME)
 
 if config.cache_github:
