fdbb62603fe89068b8bdc97c381548588e56222e
Philip Trauner <philip.trauner@arztpraxis.io>
2018-07-08 05:14:23 +0200

    Added REPL and trashed GitHub caching code + placeholder data for development

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from os.path import exists
-from os import system
-from pickle import dumps as pickle_dumps
-from pickle import loads as pickle_loads
+from types import SimpleNamespace
 
 from meh import Config, Option, ExceptionInConfigError
-from github_bridge import GitHub
-from spotify_bridge import Spotify
 from sanic import Sanic
-from sanic.response import html
+from sanic.response import html, text
 from jinja2 import Template as JinjaTemplate
-
 from spotipy import Spotify as Spotipy
 from spotipy.oauth2 import SpotifyClientCredentials as SCC
+
+from github_bridge import GitHub, Repo, Org
+from spotify_bridge import Spotify, Playlist
+from repl import Repl
 
 NAME = "philip-trauner.me"
 CONFIG_PATH = "philip-trauner.me.cfg"
@@ -23,14 +21,19 @@ PATH_HTML = "templates/index.min.html"
 config = Config()
 config += Option("address", "0.0.0.0")
 config += Option("port", 5000)
-config += Option("github_user", "PhilipTrauner")
-config += Option("spotify_user", "philip.trauner")
 config += Option("static_url", "")
 config += Option("static_handler", False, comment="Serves static files")
-config += Option("cache_github", False, comment="Caches repos and orgs")
+
+config += Option("github_user", "PhilipTrauner")
+config += Option("enable_github", True)
+
+config += Option("spotify_user", "philip.trauner")
 config += Option("spotify_client_id", None)
 config += Option("spotify_client_secret", None)
+config += Option("enable_spotify", True)
+
 config += Option("debug", False)
+config += Option("enable_repl", True)
 config += Option("additional_repos", [], comment="Additional repos")
 
 try:
@@ -41,41 +44,64 @@ except (IOError, ExceptionInConfigError):
 
 
 if config.static_url != "":
-	if config.static_url[-1] != "/":
-		config.static_url = config.static_url + "/"
+    if config.static_url[-1] != "/":
+        config.static_url = config.static_url + "/"
 
 static_url = str(config.static_url)
 
 template = JinjaTemplate(open(PATH_HTML).read())
 app = Sanic(NAME)
 
-spotify = Spotify(config.spotify_user, 
-	Spotipy(client_credentials_manager=SCC(client_id=config.spotify_client_id, 
-		client_secret=config.spotify_client_secret)))
+spotify = (
+    Spotify(
+        config.spotify_user,
+        Spotipy(
+            client_credentials_manager=SCC(
+                client_id=config.spotify_client_id,
+                client_secret=config.spotify_client_secret,
+            )
+        ),
+    )
+    if config.enable_spotify
+    else SimpleNamespace(playlists=[Playlist("Human Music", "", "")])
+)
 
-if config.cache_github:
-	if not exists(DUMP_PATH):
-		github = GitHub(config.github_user, 
-			additional_repos=config.additional_repos)
-		github.force_fetch()
-		open(DUMP_PATH, "wb").write(pickle_dumps(github))
-	else:
-		github = pickle_loads(open(DUMP_PATH, "rb").read())
-		github.disable_fetch = True
-else:
-	github = GitHub(config.github_user, additional_repos=config.additional_repos)
+github = (
+    GitHub(config.github_user, additional_repos=config.additional_repos)
+    if config.enable_github
+    else SimpleNamespace(
+        repos=[Repo("", "Name", 0, 100, True, True, "Description", None, "Brainfuck")],
+        orgs=[Org("Name", "", "Description", None)],
+    )
+)
 
 if config.static_handler:
-	app.static('/static', './static')
-	static_url = "static/"
+    app.static("/static", "./static")
+    static_url = "static/"
 
 
 @app.route("/")
 async def home(request):
-	return html(template.render(repos=github.repos, orgs=github.orgs, 
-		static_url=static_url, playlists=spotify.playlists))
+    return html(
+        template.render(
+            repos=github.repos,
+            orgs=github.orgs,
+            static_url=static_url,
+            playlists=spotify.playlists,
+        )
+    )
+
+"""
+@app.route("/blog/<post>")
+async def blog(request, post):
+    return text(post)
+"""
+
+if config.enable_repl:
+    repl = Repl()
+    repl.start()
 
 try:
-	app.run(host=config.address, port=config.port, debug=config.debug)
+    app.run(host=config.address, port=config.port, debug=config.debug)
 except KeyboardInterrupt:
-	app.stop()
+    app.stop()
