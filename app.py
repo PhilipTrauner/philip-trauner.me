7f6f8483663cd5136e55fd4e501f7d2502ee22f7
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-05 22:51:12 +0200

    Added blog related routes

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from types import SimpleNamespace
+from pathlib import Path
 
 from meh import Config, Option, ExceptionInConfigError
 from sanic import Sanic
 from sanic.response import html, text
-from jinja2 import Template as JinjaTemplate
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from spotipy import Spotify as Spotipy
 from spotipy.oauth2 import SpotifyClientCredentials as SCC
 
-from github_bridge import GitHub, Repo, Org
-from spotify_bridge import Spotify, Playlist
-from repl import Repl
+from bridges.github import GitHub, Repo, Org
+from bridges.spotify import Spotify, Playlist
+from bridges.blog import Blog
 
 NAME = "philip-trauner.me"
 CONFIG_PATH = "philip-trauner.me.cfg"
@@ -17,12 +18,16 @@ DUMP_PATH = "github-dump.pickle"
 
 PATH_HTML = "templates/index.min.html"
 
-
 config = Config()
 config += Option("address", "0.0.0.0")
 config += Option("port", 5000)
 config += Option("static_url", "")
+
 config += Option("static_handler", False, comment="Serves static files")
+
+config += Option("blog_static_url", "")
+config += Option("blog_static_handler", False, comment="Serves static files for blog")
+config += Option("rss_base_url", "https://philip-trauner.me/blog/post/")
 
 config += Option("github_user", "PhilipTrauner")
 config += Option("enable_github", True)
@@ -33,7 +38,6 @@ config += Option("spotify_client_secret", None)
 config += Option("enable_spotify", True)
 
 config += Option("debug", False)
-config += Option("enable_repl", True)
 config += Option("additional_repos", [], comment="Additional repos")
 
 try:
@@ -48,9 +52,20 @@ if config.static_url != "":
         config.static_url = config.static_url + "/"
 
 static_url = str(config.static_url)
+blog_static_url = str(config.blog_static_url)
 
-template = JinjaTemplate(open(PATH_HTML).read())
 app = Sanic(NAME)
+
+if config.static_handler:
+    app.static("/static", "./static")
+    static_url = "/static/"
+
+
+if config.blog_static_handler:
+    app.static("/blog/static/", "./posts/")
+    blog_static_url = "/blog/static/"
+
+env = Environment(loader=FileSystemLoader("templates"))
 
 spotify = (
     Spotify(
@@ -75,31 +90,50 @@ github = (
     )
 )
 
-if config.static_handler:
-    app.static("/static", "./static")
-    static_url = "static/"
+blog_ = Blog(Path("posts"), blog_static_url, config.rss_base_url)
 
 
 @app.route("/")
 async def home(request):
     return html(
-        template.render(
+        env.get_template("home.html").render(
             repos=github.repos,
             orgs=github.orgs,
             static_url=static_url,
             playlists=spotify.playlists,
+            posts=blog_.posts,
         )
     )
 
-"""
-@app.route("/blog/<post>")
-async def blog(request, post):
-    return text(post)
-"""
 
-if config.enable_repl:
-    repl = Repl()
-    repl.start()
+@app.route("/blog/post/<post>")
+async def blog_post(request, post):
+    post = blog_.find_post(post)
+
+    return html(
+        env.get_template("blog-post.html").render(
+            static_url=static_url, blog_static_url=blog_static_url, post=post
+        ),
+        status=200 if post else 404,
+    )
+
+
+@app.route("/blog/tag/<tag>")
+async def blog_tag(request, tag):
+    posts = blog_.find_posts(tag)
+
+    return html(
+        env.get_template("blog-tag.html").render(
+            static_url=static_url, posts=posts, tag=tag
+        ),
+        status=200 if len(posts) > 0 else 404,
+    )
+
+
+@app.route("/blog/rss")
+async def blog_rss(request):
+    return text(blog_.rss, headers={"Content-Type": "text/xml"})
+
 
 try:
     app.run(host=config.address, port=config.port, debug=config.debug)
