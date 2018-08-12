f8dfbc1eafe7785b87bea321eb7feaf2830cd10d
Philip Trauner <philip.trauner@arztpraxis.io>
2018-08-13 00:40:14 +0200

    Detect if post contains code, type annotations, rewrote image logic, constants for everything, sorted posts by date

--- bridges/blog.py
+++ bridges/blog.py
@@ -3,6 +3,8 @@ from threading import Lock
 from json import loads as json_loads
 from datetime import datetime
 from re import compile as re_compile
+from typing import Callable, List, Optional, Tuple, Type, Dict, Any
+
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler as WatchdogFileSystemEventHandler
@@ -18,7 +20,8 @@ from pygments.formatters import html
 from rfeed import Feed, Item, Guid
 
 WARNING_COLOR = "\033[33m"
-ANSII_END = "\033[0m"
+EMPHASIS_COLOR = "\033[35m"
+ANSI_END = "\033[0m"
 
 LICENSE_VERSION = "4.0"
 
@@ -34,16 +37,21 @@ IMAGE_REWRITE = [
     re_compile(r"(<img.*src=\")([^\"]*)(.*)"),  # <img src="image.png" />
     re_compile(r"(!\[.*\]\()([^)]*)(\))"),  # ![](image.png)
 ]
+HAS_CODE = re_compile(r"```\w+\n")
+
+CONTENT_FOLDER = "content"
+BLOG_METADATA = "metadata.json"
+BLOG_TEXT = "text.md"
 
 
-def all_match_condition(function, iterable):
+def all_match_condition(function: Callable, iterable: List[str]) -> bool:
     for element in iterable:
         if not function(element):
             return False
     return True
 
 
-def _license_urls(license_string):
+def _license_urls(license_string) -> Tuple[str, str]:
     return (
         LICENSE_BASE_URL + license_string + LICENSE_END_URL,
         LICENSE_IMAGE_BASE_URL + license_string + LICENSE_IMAGE_END_URL,
@@ -51,13 +59,13 @@ def _license_urls(license_string):
 
 
 class License:
-    def __init__(self, name, description_url, image_url):
+    def __init__(self, name: str, description_url: str, image_url: str) -> None:
         self.name = name
         self.description_url = description_url
         self.image_url = image_url
 
 
-LICENSES = {
+LICENSES: Dict[str, License] = {
     "by-nc-nd": License(
         "Attribution-NonCommercial-NoDerivatives 4.0 International",
         *_license_urls("by-nc-nd")
@@ -78,27 +86,48 @@ LICENSES = {
     "by": License("Attribution 4.0 International", *_license_urls("by")),
 }
 
-VALID_LICENSE_STRINGS = list(LICENSES.keys())
+VALID_LICENSE_STRINGS: List[str] = list(LICENSES.keys())
 
 
 class Date:
-    def __init__(self, unix_date):
+    def __init__(self, unix_date: int) -> None:
         self.unix_date = unix_date
         self.datetime = datetime.fromtimestamp(unix_date)
-        self.pretty_date = self.datetime.strftime("%Y-%m-%d")
+        self.pretty_date = self.datetime.strftime("%B %d, %Y")
         self.iso_date = self.datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Image:
+    class _Image:
+        def __init__(self, path: str, url: str) -> None:
+            self.path = path
+            self.url = url
+
+    def __new__(cls: Type["Image"], path: Path, image_base_url: Path):
+        return Image._Image(path.absolute(), str(image_base_url / path.name))
 
 
 class PostMetadata:
     class _PostMetadata:
-        def __init__(self, date, tags, license, description, section):
+        def __init__(
+            self,
+            date: Date,
+            tags: List[str],
+            license: License,
+            description: str,
+            section: str,
+            author: str,
+        ) -> None:
             self.date = date
             self.tags = tags
             self.license = license
             self.description = description
             self.section = section
+            self.author = author
 
-    def __new__(self, metadata_path):
+    def __new__(
+        cls: Type["PostMetadata"], metadata_path: Path
+    ) -> Optional["PostMetadata._PostMetadata"]:
         if PostMetadata.valid(metadata_path):
             metadata_dict = json_loads(open(metadata_path, "r").read())
             return PostMetadata._PostMetadata(
@@ -107,11 +136,12 @@ class PostMetadata:
                 LICENSES[metadata_dict["license"]],
                 metadata_dict["description"],
                 metadata_dict["section"],
+                metadata_dict["author"],
             )
         return None
 
     @staticmethod
-    def valid(metadata_path):
+    def valid(metadata_path: Path) -> bool:
         metadata_dict = json_loads(open(metadata_path, "r").read())
         return (
             "date" in metadata_dict
@@ -126,33 +156,47 @@ class PostMetadata:
             and metadata_dict["license"] in VALID_LICENSE_STRINGS
             and "description" in metadata_dict
             and "section" in metadata_dict
+            and "author" in metadata_dict
         )
 
 
 class Post:
     class _Post:
-        def __init__(self, name, title, post_metadata, markdown, images):
+        def __init__(
+            self,
+            name: str,
+            title: str,
+            post_metadata: PostMetadata._PostMetadata,
+            markdown: str,
+            images: List[Image._Image],
+            has_code: bool,
+        ) -> None:
             self.name = name
             self.title = title
             self.post_metadata = post_metadata
             self.markdown = markdown
             self.images = images
+            self.has_code = has_code
 
-    def __new__(self, post_path, image_base_url):
+    def __new__(
+        cls: Type["Post"], post_path: Path, image_base_url: str
+    ) -> "Post._Post":
         if Post.valid(post_path):
-            post_content = Post.rewrite_images(
-                open(post_path / "text.md", "r").read(),
-                image_base_url + post_path.name + "/content/",
-            )
-            post_content_split = post_content.split("\n")
+            content_path = post_path / CONTENT_FOLDER
+            image_base_url = Path(image_base_url) / post_path.name / CONTENT_FOLDER
 
-            content_path = post_path / "content"
+            unmodified_post_content = open(post_path / BLOG_TEXT, "r").read()
 
-            images = (
-                []
-                if not content_path.is_dir()
-                else Post.find_images(content_path, post_content)
+            images = []
+            if content_path.is_dir():
+                images = Post.find_images(
+                    unmodified_post_content, content_path, image_base_url
+                )
+
+            modified_post_content = Post.rewrite_images(
+                unmodified_post_content, str(image_base_url)
             )
+            post_content_split = modified_post_content.split("\n")
 
             return Post._Post(
                 post_path.name,
@@ -160,15 +204,21 @@ class Post:
                 PostMetadata(post_path / "metadata.json"),
                 Blog.MARKDOWN.render("\n".join(post_content_split[1:])),
                 images,
+                bool(HAS_CODE.search(unmodified_post_content)),
+            )
+        else:
+            print(
+                "%sWARNING%s: Validation of '%s' failed!"
+                % (WARNING_COLOR, ANSI_END, post_path)
             )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<Post title="%s" name="%s" id=%s>' % (self.title, self.name, id(self))
 
     @staticmethod
-    def valid(post_path):
-        text_path = post_path / "text.md"
-        metadata_path = post_path / "metadata.json"
+    def valid(post_path: Path) -> bool:
+        text_path = post_path / BLOG_TEXT
+        metadata_path = post_path / BLOG_METADATA
 
         valid_text = False
         valid_metadata = metadata_path.exists() and PostMetadata.valid(metadata_path)
@@ -181,34 +231,40 @@ class Post:
         return valid_text and valid_metadata
 
     @staticmethod
-    def rewrite_images(post_content, image_base_url):
+    def rewrite_images(post_content: str, image_base_url: str) -> str:
         for regex in IMAGE_REWRITE:
-            post_content = regex.sub("\g<1>%s\g<2>\g<3>" % image_base_url, post_content)
+            post_content = regex.sub(
+                "\g<1>%s/\g<2>\g<3>" % image_base_url, post_content
+            )
 
         return post_content
 
     @staticmethod
-    def find_images(content_path, post_content):
+    def find_images(
+        post_content: str, content_path: Path, image_base_url: Path
+    ) -> List[Image._Image]:
         images = [
-            image_tuple[0] if image_tuple[0] != "" else image_tuple[1]
+            Image(
+                content_path
+                / Path(image_tuple[0] if image_tuple[0] else image_tuple[1]),
+                image_base_url,
+            )
             for image_tuple in FIND_IMAGE.findall(post_content)
         ]
 
-        """
         for image in images:
-            if not (content_path / image).is_file():
+            if not image.path.is_file():
                 print(
-                    "%sWARNING%s: '%s' not found in post content! (%s)"
-                    % (WARNING_COLOR, ANSII_END, image, content_path)
+                    "%sWARNING%s: '%s' does not exist!"
+                    % (WARNING_COLOR, ANSI_END, image.path)
                 )
-        """
 
         return images
 
 
 class Blog:
     class _HighlightRenderer(MistuneRenderer):
-        def block_code(self, code, lang):
+        def block_code(self, code: str, lang: Optional[str]) -> str:
             if not lang:
                 return "\n<pre><code>%s</code></pre>\n" % mistune_escape(code)
             lexer = get_lexer_by_name(lang, stripall=True)
@@ -216,35 +272,39 @@ class Blog:
             return highlight(code, lexer, formatter)
 
     class _FileSystemEventHandler(WatchdogFileSystemEventHandler):
-        def __init__(self, blog):
-            self.blog = blog
+        def __init__(self, method: Callable):
+            self.method = method
 
-        def on_any_event(self, event):
-            self.blog._Blog__refresh_posts()
+        def on_any_event(self, event: Any):
+            self.method()
 
     MARKDOWN = MistuneMarkdown(renderer=_HighlightRenderer())
 
-    def __init__(self, path=Path("blog"), image_base_url="", rss_base_url=""):
+    def __init__(
+        self, path: Path, image_base_url: str, rss_base_url: str, rss_url: str
+    ) -> None:
         self.path = path
         self.image_base_url = image_base_url
         self.rss_base_url = rss_base_url
+        self.rss_url = rss_url
 
         self.observer = Observer()
         self.observer.schedule(
-            Blog._FileSystemEventHandler(self), str(path.absolute()), recursive=True
+            Blog._FileSystemEventHandler(self.__refresh_posts),
+            str(path.absolute()),
+            recursive=True,
         )
         self.observer.start()
 
         self._posts = []
         self._tags = {}
         self._rss = ""
-        self._atom = ""
 
         self.posts_lock = Lock()
 
         self.__refresh_posts()
 
-    def find_post(self, name):
+    def find_post(self, name: str) -> Optional[Post._Post]:
         post = None
 
         self.posts_lock.acquire()
@@ -256,7 +316,7 @@ class Blog:
 
         return post
 
-    def find_posts(self, tag):
+    def find_posts(self, tag: str) -> List[Post._Post]:
         posts = []
 
         self.posts_lock.acquire()
@@ -268,7 +328,7 @@ class Blog:
         return posts
 
     @property
-    def posts(self):
+    def posts(self) -> List[Post._Post]:
         self.posts_lock.acquire()
         posts = self._posts[:]
         self.posts_lock.release()
@@ -276,7 +336,7 @@ class Blog:
         return self._posts
 
     @property
-    def tags(self):
+    def tags(self) -> Dict[str, List[Post._Post]]:
         self.posts_lock.acquire()
         tags = self._tags.copy()
         self.posts_lock.release()
@@ -284,17 +344,15 @@ class Blog:
         return self._tags
 
     @property
-    def rss(self):
+    def rss(self) -> str:
         self.posts_lock.acquire()
         rss = self._rss
         self.posts_lock.release()
 
         return rss
 
-    def __refresh_posts(self):
-        self.posts_lock.acquire()
-
-        print("Refreshing!")
+    def __refresh_posts(self) -> None:
+        print("%sINFO%s: Refreshing!" % (EMPHASIS_COLOR, ANSI_END))
 
         posts = []
         tags = {}
@@ -305,14 +363,14 @@ class Blog:
             if folder.is_dir() and Post.valid(folder):
                 post = Post(folder, self.image_base_url)
 
-                link = "%s%s" % (self.rss_base_url, post.name)
+                link = str(Path(self.rss_base_url) / post.name)
 
                 feed_items.append(
                     Item(
                         title=post.title,
                         link=link,
                         description=post.post_metadata.description,
-                        author="Philip Trauner",
+                        author=post.post_metadata.author,
                         guid=Guid(link),
                         pubDate=post.post_metadata.date.datetime,
                     )
@@ -327,14 +385,18 @@ class Blog:
 
         feed = Feed(
             title="Philip Trauner",
-            link="https://philip-trauner.me/blog/rss",
+            link=self.rss_url,
             description="",
             language="en-US",
             lastBuildDate=datetime.now(),
             items=feed_items,
         )
 
-        self._posts = posts
+        self.posts_lock.acquire()
+
+        self._posts = sorted(
+            posts, key=lambda post: post.post_metadata.date.unix_date, reverse=True
+        )
         self._tags = tags
         self._rss = feed.rss()
 
